152c7e7e3966e1d56e5801634afff7ce34395e72
Pranab <72617824+PranabZz@users.noreply.github.com>
2022-07-31 06:28:29 +0545

    kjaslkdj

--- CSIT/CG/area_filling_circle.cpp
+++ CSIT/CG/area_filling_circle.cpp
@@ -3,28 +3,31 @@ using namespace std;
 
 int main()
 {
-    int r, x, y, f, x1, y1;
+    int r, x, y, f, x1, y1,i;
 
     cout << "Enter the points and radius" << endl;
     cin >> x >> y >> r;
 
-    
-
+    f = (x * x) + (y * y) - (r * r);
+    i = 0;
     do
     {
-        f = (x * x) + (y * y) - (r * r);
 
         if (f <= 0)
         {
             x1 = x;
             y1 = y + 1;
             cout << x1 << ", " << y1 << endl;
+            f = f + (2*x1) + 3; 
         }
         else
         {
             x1 = x - 1;
             y1 = y + 1;
             cout << x1 << ", " << y1 << endl;
+            f = f + (2*x1) -(2*y1)+ 5; 
         }
-    }while (x1 == x && y1 == y);
+
+        i++;
+    }while (i < 4);
 }
